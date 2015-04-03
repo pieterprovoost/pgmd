c09986cf77d4262219529155c67449d8bc4821f4
unknown <pieterprovoost@gmail.com>
2015-04-03 14:51:46 +0200

    breadcrumbs

--- pgmd.py
+++ pgmd.py
@@ -20,6 +20,11 @@ except:
 def formatc(source):
 	return '    ' + '    '.join(source.splitlines(True))
 
+# create link
+
+def link(name, url):
+	return '[' + name + '](' + url + ')'
+
 # excludes
 
 excludes = tuple(item.strip() for item in parser.get('schema', 'exclude').split(','))
@@ -31,6 +36,10 @@ script_tables = open("tables.sql", "r").read()
 script_views = open("views.sql", "r").read()
 script_routines = open("routines.sql", "r").read()
 script_columns = open("columns.sql", "r").read()
+
+# database
+
+dbname = parser.get('database', 'dbname')
 
 # schemas
 
@@ -78,30 +87,40 @@ else:
 # create index file
 
 f = open('output/index.md', 'w')
-f.write('# ' + parser.get('database', 'dbname') + '\n')
+f.write('# ' + dbname + '\n')
 f.write('##  schemas\n')
 
 # schema list
 
 for schema in [s[0] for s in schemas]:
-	f.write('[' + schema + '](#' + schema + ')  \n')
+	f.write(link(schema, schema) + '  \n')
 
 # schemas
 
 for schema in [s[0] for s in schemas]:
-	f.write('<a name="' + schema + '"></a>\n')
-	f.write('## ' + schema + '\n')
+
+	# schema file
+
+	fs = open('output/' + schema + '.md', 'w')
+	fs.write('# ' + schema + '\n')
+	fs.write('database: ' + link(dbname, '../') + '  \n')
 
 	# tables
 
-	f.write('### tables\n')
+	fs.write('## tables\n')
 	schematables = [table for table in tables if table[0] == schema]
 	for table in schematables:
 		tablename = table[1]
-		f.write('[' + tablename + '](' + schema + '_' + tablename + '_table)  \n')
+		fs.write(link(tablename, schema + '_' + tablename + '_table') + '  \n')
+
+		# table file
 
 		ft = open('output/' + schema + '_' + tablename + '_table.md', 'w')
+
 		ft.write('# ' + tablename + '\n')
+		ft.write('database: ' + link(dbname, '../') + '  \n')
+		ft.write('schema: ' + link(schema, schema) + '  \n\n')
+
 		ft.write('|Column|Type|\n')
 		ft.write('|:---|:---|\n')
 		tablecolumns = [column for column in columns if column[1] == tablename]
@@ -113,33 +132,47 @@ for schema in [s[0] for s in schemas]:
 
 	# views
 
-	f.write('### views\n')
+	fs.write('## views\n')
 	schemaviews = [view for view in views if view[0] == schema]
 	for view in schemaviews:
 		viewname = view[1]
 		viewdef = view[2]
-		f.write('[' + viewname + '](' + schema + '_' + viewname + '_view)  \n')
+		fs.write(link(viewname, schema + '_' + viewname + '_view') + '  \n')
+
+		# view file
 
 		fr = open('output/' + schema + '_' + viewname + '_view.md', 'w')
 		fr.write('# ' + viewname + '\n')
+		fr.write('database: ' + link(dbname, '../') + '  \n')
+		fr.write('schema: ' + link(schema, schema) + '  \n\n')
+
 		fr.write(formatc(viewdef))
 		fr.close()
 
 	# routines
 
-	f.write('### routines\n')
+	fs.write('## routines\n')
 	schemaroutines = [routine for routine in routines if routine[0] == schema]
 	for routine in schemaroutines:
 		routinename = routine[1]
 		routinelang = routine[5]
 		routinesrc = routine[2]
 		if routinename is not None:
-			f.write('[' + routinename + '](' + schema + '_' + routinename + '_routine) ' + routinelang + '  \n')
+			fs.write(link(routinename, schema + '_' + routinename + '_routine') + '<span class="lang">' + routinelang + '</span>  \n')
+
+			# routine file
 
 			fr = open('output/' + schema + '_' + routinename + '_routine.md', 'w')
 			fr.write('# ' + routinename + '\n')
+			fr.write('database: ' + link(dbname, '../') + '  \n')
+			fr.write('schema: ' + link(schema, schema) + '  \n\n')
+
 			fr.write(formatc(routinesrc))
 			fr.close()
+
+	# close schema file
+
+	fs.close()
 
 # close index file
 
